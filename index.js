193283aedcb2b1a3079df40524c9dfb6885326f8
vanshaj810 <vanshaj810@gmail.com>
2021-09-14 12:59:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+
+const users = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+    },
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    address: {
+      street: 'Victor Plains',
+      suite: 'Suite 879',
+      city: 'Wisokyburgh',
+      zipcode: '90566-7771',
+    },
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    address: {
+      street: 'Douglas Extension',
+      suite: 'Suite 847',
+      city: 'McKenziehaven',
+      zipcode: '59590-4157',
+    },
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    address: {
+      street: 'Hoeger Mall',
+      suite: 'Apt. 692',
+      city: 'South Elvis',
+      zipcode: '53919-4257',
+    },
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    address: {
+      street: 'Skiles Walks',
+      suite: 'Suite 351',
+      city: 'Roscoeview',
+      zipcode: '33263',
+    },
+  },
+];
+
+app.get('/', (_request, response) => {
+  response.send('<h1>Hello Vanshaj</h1>');
+});
+
+app.get('/users', (_request, response) => {
+  response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users.find((usr) => usr.id === id);
+
+  if (user) {
+    response.json(user);
+  } else {
+    response.status(404).json({ message: 'User not found', status: 404 });
+  }
+});
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
